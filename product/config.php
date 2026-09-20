d196ad2ac715b4f8c90cde351b1a61aa977fd369
rayhan <essop1007@gmail.com>
2026-09-20 14:25:45 +0530

    update

--- product/config.php
+++ product/config.php
@@ -1,6 +1,21 @@
 <?php
 // Database connection settings
-$con = mysqli_connect("localhost", "root", "", "ecommerce");
+
+if (isset($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] === 'mystore-rayhan.infinityfree.io') {
+    // InfinityFree
+    $host = "sql313.infinityfree.com";
+    $user = "if0_42963174";
+    $password = "MOTALA10";
+    $database = "if0_42963174_ecommerce";
+} else {
+    // Local XAMPP
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $database = "ecommerce";
+}
+
+$con = mysqli_connect($host, $user, $password, $database);
 
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
