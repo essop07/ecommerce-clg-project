e35a33e3fe2ce85098cf1bbf2d5918b03bef8879
rayhan <essop1007@gmail.com>
2026-09-20 15:12:50 +0530

    update

--- product/config.php
+++ product/config.php
@@ -1,12 +1,25 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "ecommerce";
+// Detect whether we're running locally (XAMPP) or on the live server
+$isLocal = in_array($_SERVER['SERVER_NAME'] ?? '', ['localhost', '127.0.0.1'], true);
 
-$con = mysqli_connect($host, $user, $password, $database);
+if ($isLocal) {
+    // XAMPP
+    $host     = "localhost";
+    $user     = "root";
+    $password = "";
+    $database = "ecommerce";
+} else {
+    // InfinityFree
+    $host     = "sql313.infinityfree.com";
+    $user     = "if0_42963174";
+    $password = "MOTALA10"; // <- paste it here (click the eye icon to reveal it)
+    $database = "if0_42963174_ecommerce";
+}
+
+$con = mysqli_connect($host, $user, $password, $database, 3306);
 
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
-?>
+
+mysqli_set_charset($con, "utf8mb4");
